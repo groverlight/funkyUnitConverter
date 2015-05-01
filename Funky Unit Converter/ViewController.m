a12d5ab06e552cf499455bca28e51c92d0864384
Grover Light <grover.light@gmail.com>
2015-05-01 15:24:19 -0700

    fixed missing token

--- Funky Unit Converter/ViewController.m
+++ Funky Unit Converter/ViewController.m
@@ -17,16 +17,19 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
-    NSLog(@"Hello World");
 
+    int w = 42;
     int x = 5;
     int y = 20;
     int z = -2;
 
+    NSLog(@"The value of w is %i. The value of x is %i. The value of y is %i. The value of z is %i.", w,x,y,z);
+
+
     //operators include: +, -, *, /
 
     int addtionAnswer = x + y;
-    int multiplicationAnswer = y * z;
+    int multiplicationAnswer = y * z * w ;
 
     float heightOfEverestBaseCamp = 16900.3;
     float heightOFEverest = 29029;
@@ -34,6 +37,7 @@
     float distanceToTravel = heightOFEverest - heightOfEverestBaseCamp;
     distanceToTravel = distanceToTravel - 1000;
 
+    NSLog(@"The distance from bottom of Everst to the top of Everst is %f", distanceToTravel);
 
 
 }
